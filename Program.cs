fb3fde21dff8bd14533df13a242d8e46a14b3dc8
Carlos Rony <carlosrecla03@gmail.com>
2021-11-03 15:48:03 -0300

    final da tarefa

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaiorNumereo
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+            int primeiro, segundo, MaiorNumero;
+            
+            Console.Write("Digite o primeiro número....:");
+            primeiro = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Digite o segundo número.....:");
+            segundo = Convert.ToInt32(Console.ReadLine());
+
+            if (primeiro > segundo)
+            {
+                MaiorNumero = primeiro;
+            }
+            else
+            {
+                MaiorNumero = segundo;
+            }   
+            Console.WriteLine($"o Maior Número é {MaiorNumero}"); 
+       }
+    }   
+}    
